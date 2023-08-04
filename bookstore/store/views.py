d70af9e4067902701cfff1cb0f8d7bb172d300fc
Furkan Alkılıç <fbfurkan2018@gmail.com>
2023-08-04 15:13:58 +0300

    Cleaned up some code.

--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -1,13 +1,13 @@
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
-from registration.models import User
 #
 from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 #
 from .serializer import UserSerializer, BookSerializer, BorrowerSerializer
+from registration.models import User
 from .models import Book, Borrower
 import datetime
         
